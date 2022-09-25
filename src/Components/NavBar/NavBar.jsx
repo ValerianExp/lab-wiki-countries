89da2d44c2a4e405c65d3a045b852fb25ce78a93
RedValerian <r.pino.camacho@gmail.com>
2022-09-25 11:11:46 +0200

    Routes need fixing

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
 function NavBar() {
     return (
         <nav className="navbar navbar-dark bg-dark" >
             <div className="container">
-                <NavLink className="navbar-brand" to="/">WikiContries</NavLink>
+                <NavLink className="navbar-brand">
+                    {/* <Routes>
+                        <Route path="/" element={<h1>WikiCountries</h1>} />
+                    </Routes> */}
+                    WikiCountries
+                </NavLink>
+                {/* <Routes>
+                    <Route path="/" element={<NavLink className="navbar-brand" to="/">WikiCountries</NavLink>} />
+                </Routes> */}
             </div>
         </nav >
     );
